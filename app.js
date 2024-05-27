f6594d7e8b5b63f43173f0c73a105a9c2fd06b5b
aBiteOfPizza <ssys0729@gmail.com>
2024-05-27 13:25:52 +0900

    branch commit

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.get('/', (req,res) => {
     res.end('<h1> Hello World </h1>');
 });
 
+app.get('home', (req,res) => {
+    res.end('<h1> Home Sweet Home </h1>');
+});
+
 const server = http.createServer(app);
 server.listen(3000, () => {
     console.log("Run on server http://localhost:3000");
 });
+
